b2a6cc987a1655482476e4ac71ec1cdbf7f3562e
Dietrich Liko <Dietrich.Liko@oeaw.ac.at>
2019-04-17 15:04:27 +0200

    Add test for file on CVMFS ...

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_package(host):
+    pkg = host.package('cvmfs')
+
+    assert pkg.is_installed
+
+def test_file_on_cvmfs(host):
+    f = host.file('/cvmfs/cms.cern.ch/SITECONF/local/JobConfig/site-local-config.xml')
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == 'cvmfs'
+    assert f.group == 'cvmfs'
